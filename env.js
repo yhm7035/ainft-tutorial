bb6d4b7dfef38f63e807752907a3a39566497da8
Hongmin <hongmin@Hongminui-MacBookPro.local>
2023-02-24 10:44:18 +0900

    [feature] change ain app

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const env = {
   AIN_API_SERVER: 'https://testnet-api.ainetwork.ai',
-  APP_NAME: 'ainft_for_comcom',
-  APP_PATH: '/apps/ainft_for_comcom'
+  APP_NAME: 'ainft_tutorial_dev',
+  APP_PATH: '/apps/ainft_tutorial_dev'
 }
